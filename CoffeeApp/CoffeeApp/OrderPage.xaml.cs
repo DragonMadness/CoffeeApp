9e1b3a9484af059ffdf29f326c7b2361fec48b11
student <student@MGOK-V-201-25N>
2023-11-30 17:43:26 +0300

    update 0.1

--- CoffeeApp/CoffeeApp/OrderPage.xaml.cs
+++ CoffeeApp/CoffeeApp/OrderPage.xaml.cs
@@ -13,13 +13,14 @@ namespace CoffeeApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class OrderPage : ContentPage
 	{
-
+		public List<Building> Buildings { get; set; }
 		public List<Product> Catalog {  get; set; }
 
 		public OrderPage ()
 		{
             Title = "Создание заказа";
-            Catalog = new List<Product>() { new Product("Американо", 30) };
+            Catalog = new List<Product>() { new Product("Американо", 30), new  Product("Латте", 40) };
+            Buildings = new List<Building>() { new Building("Moscow", "09.00-19.00", new List<Product> { new Product("Латте", 10) }) };
             InitializeComponent ();
 			this.BindingContext = this;
 		}
